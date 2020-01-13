e1f0b78ba32212fa99271f81d851bb9b2f61f835
Erwin LEBRETON <erwinlebreton@gmail.com>
2020-01-13 16:32:48 +0100

    Version BUG SLIDE

--- app/src/main/java/com/mds/sorbet/MainActivity.java
+++ app/src/main/java/com/mds/sorbet/MainActivity.java
@@ -1,14 +1,19 @@
 package com.mds.sorbet;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import androidx.viewpager.widget.ViewPager;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
+    private ViewPager viewPager;
+    private SlideAdapter myadapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        viewPager = (ViewPager) findViewById(R.id.viewpager);
+        myadapter = new SlideAdapter(this);
+        viewPager.setAdapter(myadapter);
+
     }
 }
