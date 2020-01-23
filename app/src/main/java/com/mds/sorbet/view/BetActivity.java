db6642c384547cd3fb9b838e1aa1c62ca4923a1d
Erwin LEBRETON <erwinlebreton@gmail.com>
2020-01-23 09:14:10 +0100

    All Svg + BetActivity

---
+++ app/src/main/java/com/mds/sorbet/view/BetActivity.java
@@ -0,0 +1,14 @@
+package com.mds.sorbet.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+import com.mds.sorbet.R;
+import android.os.Bundle;
+
+public class BetActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bet);
+    }
+}
